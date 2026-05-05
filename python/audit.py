9cdaaec1b4ad6e3c802e87a96b22e83468f2e34a
hoyjdev <nick@hoyj.dev>
2026-05-04 23:02:11 -0700

    Extract helper functions and update flow to shell core shell core shell

--- python/audit.py
+++ python/audit.py
@@ -2,7 +2,6 @@ import abc
 import os.path
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
 
 
 class FileSystem(abc.ABC):
@@ -30,35 +29,19 @@ class AuditManager:
     def add_record(self, visitor_name: str, time_of_visit: datetime):
         # Fetch file paths
         file_paths = self._file_system.get_files(self._directory_name)
-        # Sorted paths by index, lowest to highest. [(1, 'audits/audit_1.txt'), (2, 'audits/audit_2.txt')]
-        sorted_paths = self._sort_by_index(file_paths)
-        # Create entry
-        entry = visitor_name + ";" + time_of_visit.strftime("%Y-%m-%d %H:%M:%S")
 
-        if len(sorted_paths) == 0:
-            # Edge case: no paths, then create one file and write
-            new_file = os.path.join(self._directory_name, "audit_1.txt")
-            self._file_system.write_all_text(new_file, entry)
-            return
+        sorted_paths = sort_by_index(file_paths)
+        last_path = find_last_path(sorted_paths)
 
-        # Otherwise: Read all lines from last file
-        current_file_index, curr_file_path = sorted_paths[-1]
-        lines = self._file_system.read_all_lines(curr_file_path)
+        # Read entries from last file (in reality, this would default to empty list)
+        lines = self._file_system.read_all_lines(last_path[1])
 
-        record = Record(current_file_index, lines)
+        entry = create_entry(visitor_name, time_of_visit)
+        record = Record(last_path[0], lines)
         record = push_or_create_new(record, entry, self._max_entries_per_file)
 
-        if len(lines) < self._max_entries_per_file:
-            # If there are fewer lines than max, write to last file
-            self._file_system.write_all_text(curr_file_path, str(record))
-        else:
-            # If there are more lines than max, write to new file
-            new_file = os.path.join(self._directory_name, f"audit_{record.index}.txt")
-            self._file_system.write_all_text(new_file, str(record))
-
-    @staticmethod
-    def _sort_by_index(file_paths) -> list[tuple[Any, Any]]:
-        return list(enumerate(sorted(file_paths), start=1))
+        new_file = os.path.join(self._directory_name, f"audit_{record.index}.txt")
+        self._file_system.write_all_text(new_file, str(record))
 
 
 @dataclass
@@ -74,3 +57,17 @@ def push_or_create_new(record: Record, item: str, max_size: int) -> Record:
     if len(record.entries) == max_size:
         return Record(record.index + 1, [item])
     return Record(record.index, record.entries + [item])
+
+
+def sort_by_index(file_paths) -> list[tuple[int, str]]:
+    # Sort paths by index, lowest to highest. [(1, 'audits/audit_1.txt'), (2, 'audits/audit_2.txt')]
+    return list(enumerate(sorted(file_paths), start=1))
+
+
+def find_last_path(paths: list[tuple[int, str]]) -> tuple[int, str]:
+    # Return last index and path in a list, or a default
+    return next(iter(reversed(paths)), (1, ""))
+
+
+def create_entry(visitor_name: str, time_of_visit: datetime) -> str:
+    return visitor_name + ";" + time_of_visit.strftime("%Y-%m-%d %H:%M:%S")
